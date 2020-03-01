222ab0c041849a32e0890b76d6c934055baffaea
Harry Jiang <jxx1998@gmail.com>
2020-03-01 13:59:20 -0800

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from chatbot import Chatbot
-
-bot = Chatbot()
-line = 'I loved "10 Things I Hate About You"'
-
-# print(bot.extract_sentiment(line))
-print(bot.find_movies_closest_to_title('Blargdeblargh', 4))
